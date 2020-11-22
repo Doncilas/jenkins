e5cc64de37afec63b99e68f40d3e47459bdcb348
Sergiu Doncila <sergiu.doncila@yahoo.com>
2020-11-22 14:07:31 +0200

    jenkins testing

---
+++ test_tutorial.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from tutorial import FLaskOpen
+import pdb
+
+class TestFlaskOpen(TestCase):
+
+    def setUp(self):
+        pass
+
+
+    def test_suma_zarurilor(self):
+        theflask = FLaskOpen()
+        pdb.set_trace()
+        print('Hey')

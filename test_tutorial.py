1a2db4c8ed5dc4c1d6a97ffd333df4602bbf4083
Doncilas <52882237+Doncilas@users.noreply.github.com>
2020-11-23 10:47:15 +0200

    Update test_tutorial.py

--- test_tutorial.py
+++ test_tutorial.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 from tutorial import FLaskOpen
-import pdb
 
 class TestFlaskOpen(TestCase):
 
@@ -9,6 +8,4 @@ class TestFlaskOpen(TestCase):
 
 
     def test_suma_zarurilor(self):
-        theflask = FLaskOpen()
-        pdb.set_trace()
-        print('Hey')
+        theflask = FLaskOpen(

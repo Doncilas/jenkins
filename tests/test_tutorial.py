5dc07799912ee8c65997650e9ba9432d2baab90c
Doncilas <52882237+Doncilas@users.noreply.github.com>
2020-11-24 17:57:03 +0200

    Update test_tutorial.py

--- tests/test_tutorial.py
+++ tests/test_tutorial.py
@@ -1,12 +1,11 @@
 from unittest import TestCase
-from tutorial import FLaskOpen
+from ..tutorial import FLaskOpen
 
 class TestFlaskOpen(TestCase):
 
     def setUp(self):
         pass
-
-
+    
     def test_suma_zarurilor(self):
         theflask = FLaskOpen()
         print('hau')

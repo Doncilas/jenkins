acf813cd7dbb3b35dafd7ad04c8464d2856e8f20
Sergiu Doncila <sergiu.doncila@yahoo.com>
2020-11-24 18:27:28 +0200

    xiaomi

---
+++ tests/test_tuto2.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from ..tuto2 import Xiaomi
+
+class TestXiaomi(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_phone_number(self):
+        theflask = Xiaomi()
